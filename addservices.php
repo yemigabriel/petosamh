f94114f5e86becc242cd16811cab2042087a79a8
yemigabriel <yemigabriel@gmail.com>
2015-01-10 08:50:16 +0100

    hr
    tuu

---
+++ addservices.php
@@ -0,0 +1,341 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<link rel="shortcut icon" href="../favicon.ico" type="image/x-icon"/>
+        <link rel="stylesheet" href="css/style.css" type="text/css" media="screen"/>
+		<script src="js/cufon-yui.js" type="text/javascript"></script>
+		<script src="js/Aller.font.js" type="text/javascript"></script>
+		<script type="text/javascript">
+			Cufon.replace('ul.oe_menu div a',{hover: true});
+			Cufon.replace('h1,h2,.oe_heading');
+		</script>
+        <style type="text/css">
+			span.reference{
+				position:fixed;
+				left:0px;
+				bottom:0px;
+				background:#000;
+				width:100%;
+				font-size:10px;
+				line-height:20px;
+				text-align:right;
+				height:20px;
+				-moz-box-shadow:-1px 0px 10px #000;
+				-webkit-box-shadow:-1px 0px 10px #000;
+				box-shadow:-1px 0px 10px #000;
+			}
+			span.reference a{
+				color:#aaa;
+				text-transform:uppercase;
+				text-decoration:none;
+				margin-right:10px;
+				
+			}
+			span.reference a:hover{
+				color:#ddd;
+			}
+			.bg_img img{
+				width:100%;
+				position:fixed;
+				top:0px;
+				left:0px;
+				z-index:-1;
+			}
+			h1{
+				font-size:75px;
+				text-align:right;
+				position:absolute;
+				right:40px;
+				top:20px;
+				font-weight:normal;
+				/*text-shadow:  0 0 3px #0096ff, 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #0096ff, 0 0 70px #0096ff, 0 0 80px #0096ff, 0 0 100px #0096ff, 0 0 150px #0096ff;
+			*/}
+			h1 span{
+				display:block;
+				font-size:15px;
+				font-weight:bold;
+			}
+			h2{
+				position:absolute;
+				top:220px;
+				left:50px;
+				font-size:40px;
+				font-weight:normal;
+				/*text-shadow:  0 0 3px #f6ff00, 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #f6ff00, 0 0 70px #f6ff00, 0 0 80px #f6ff00, 0 0 100px #f6ff00, 0 0 150px #f6ff00;
+*/}
+		</style>
+
+
+<title>Petosam Integrated Services Ltd ::: Electrical Engineering Works</title>
+<link rel="stylesheet" type="text/css" href="./index.css" media="all" />
+<!--[if IE]>
+<style type="text/css" media="all">.borderitem {border-style:solid;}</style>
+<![endif]-->
+<script type="text/javascript" src="style/js/scripts.js"></script>
+
+<script type="text/javascript" src="Scripts/swfobject/swfobject.js"></script>
+<script src="SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
+<script type="text/javascript">
+  var flashvars = {};
+  flashvars.cssSource = "piecemaker.css";
+  flashvars.xmlSource = "piecemaker.xml";
+	
+  var params = {};
+  params.play = "true";
+  params.menu = "true";
+  params.scale = "showall";
+  params.wmode = "transparent";
+  params.allowfullscreen = "true";
+  params.allowscriptaccess = "always";
+  params.allownetworking = "all";
+  
+  swfobject.embedSWF('piecemaker.swf', 'piecemaker', '850', '270', '1', null, flashvars,    
+  params, null);
+
+</script>
+
+<link href="SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
+<link href="SpryAssets/SpryMenuBarVertical.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body class="bodymain">
+<div id="main">
+	<div id="HeaderWrap">
+    
+	 <div id="menu">
+<img src="images/hdr1.png" width="286" height="101"  class="search" />
+	<span class="moveleft"><img src="mail.png" width="25" height="24" /><a href="mail.petosam.com">Webmail</a><br/>
+	info@petosam.com
+<br/> +234 803 641 5205</span>
+    <br class="clearfloat" />
+    
+<div class="oe_wrapper">
+			<div id="oe_overlay" class="oe_overlay"></div>
+			<ul id="oe_menu" class="oe_menu">
+				<li><a href="index.php">Home</a>
+				
+				</li>
+				<li><a href="aboutus.php">About us</a>
+					<div style="left:-111px;"><!-- -112px -->
+						<ul class="oe_full">
+							<li class="oe_heading">Who we are</li>
+							<li><p>Established since July 2001, Petosam Integrated Services Limited is an indigenous, innovative and dynamic company rendering professional Electrical &amp; Instrumentation Engineering, Civil Engineering, Procurement and Construction, Power and Safety System Control and consultancy services <a href="aboutus.php">more</a></p></li>
+						</ul>
+						
+					</div>
+				</li><li><a href="#">Services</a>
+					<div style="left:-223px;">
+						<ul>
+							<li class="oe_heading" ><a href="eleceng.php">Electrical  &amp; Instrumentation</a></li>
+							
+						</ul><ul>
+							<li class="oe_heading"><a href="pipe.php">Civil &amp; Pipeline Construction</a></li>
+							
+						</ul>
+                        <ul>
+							<li class="oe_heading"><a href="procure.php">Engineering Procurement</a></li>
+							
+							
+						</ul>
+						<ul>
+							<li class="oe_heading"><a href="powereng.php">Power Systems Engineering &amp; Support</a></li>
+							
+							
+						</ul>
+                        <ul>
+							<li class="oe_heading"><a href="lease.php">Lease &amp; Brokerage</a></li>
+							
+							
+						</ul>
+                        <ul>
+							<li class="oe_heading"><a href="engdesign.php">Engineering Design</a></li>
+							
+							
+						</ul>
+					</div>
+				</li>
+
+				<li><a href="index.php">Resources</a>
+				
+				</li>
+                
+                <li><a href="ourprojects.php">Projects</a>
+		
+				</li>
+                
+                <li><a href="index.php">QHSE</a>
+		
+				</li>
+                
+                <li><a href="index.php">Careers</a>
+		
+				</li>
+
+
+				<li><a href="contactus.php">Contact us</a>
+					<div style="left:-782px;">
+						<ul>
+							<li class="oe_heading" >Address</li>
+							<li><p>17 Hospital Road, opp. Ekpan Secondary School, Ekpan,
+Delta State, Nigeria.
+</p></li>
+						</ul>
+                        <ul>
+							<li class="oe_heading">Phone</li>
+							<li><p>+234 80209 88314<br />
++234 80364 15205</p></li>
+						</ul>
+						<ul>
+							<li class="oe_heading">Social</li>
+							<li><a href="#"><img src="images/facebook-logo-webtreats.png" width="56" height="56" alt="facebook" /></a>
+            <a href="#"><img src="images/twitter-webtreats.png" width="56" height="56" alt="twitter" /></a>
+           
+            <p>info@petosam.com</p></li>
+							
+						</ul>
+						
+					</div>
+				</li>
+<!--				<li><a href="">Stores</a>
+					<div style="left:-447px;">
+						<ul>
+							<li class="oe_heading">Europe</li>
+							<li><a href="#">Milano</a></li>
+							<li><a href="#">Paris</a></li>
+							<li><a href="#">Berlin</a></li>
+							<li><a href="#">London</a></li>
+						</ul>
+						<ul>
+							<li class="oe_heading">Asia</li>
+							<li><a href="#">Hong Kong</a></li>
+							<li><a href="#">Tokio</a></li>
+							<li><a href="#">New Delhi</a></li>
+							<li><a href="#">Beijing</a></li>
+						</ul>
+						<ul>
+							<li class="oe_heading">United States</li>
+							<li><a href="#">New York</a></li>
+							<li><a href="#">Los Angeles</a></li>
+							<li><a href="#">Seattle</a></li>
+							<li><a href="#">Miami</a></li>
+						</ul>
+					</div>
+				</li>
+-->			
+		
+			</ul>	
+		</div>
+       
+ 
+	</div>
+    </div>
+	<br class="clearfloat" />
+
+	<div id="DivWrap">
+		<div id="content">
+        <p class="headertext">Electrical & Instrumentation
+
+        <p><br />
+          <strong>Additional Services</strong><br />      
+          <li>Commercial work</li>
+          <li>Condos/apartment complexes</li>
+          <li>Churches</li>
+          <li>Remodels</li>
+          
+            <li>On retrofits and remodels        CE&amp;I can work around the clock to have minimum disturbance during        business hours</li>
+          
+          <li>Office buildings solutions</li>
+          <li>Electrical and lighting design</li>
+          <li>Design/build</li>
+          <li>Bid and spec work</li>
+          <li>Gas stations</li>
+          <li>Schools (High School, Jr. High,       and Elementary)</li>
+          <li>New construction, remodels,       additions solutions</li>
+          <li>Retrofit fire alarm systems</li>
+          <li>Control systems for HVAC and       environmental systems</li>
+          <li>Energy management systems for       HVAC and lighting control</li>
+          <li>Lighting retrofits (reduce       power consumption)</li>
+          <li>New fluorescent technology</li>
+          <li>Variable speed drives on air       handling equipment and environmental controls</li>
+          <li>Lighting maintenance - parking       lot lighting maintenance</li>
+          <li>Theaters (sound, lighting, and       integrated systems)</li>
+          <li>High end residential building       solutions</li>
+        </ul>
+<p>&nbsp;</p>
+	  </div>
+		<div id="sidebar">
+        <p>
+        <br /><p>Electrical &amp; Instrumentation:</p>
+        <ul id="MenuBar1" class="MenuBarVertical">
+          <li><a href="eleceng.php">Electrical</a>          </li>
+          <li><a href="inst.php">Instrumentation</a></li>
+          <li><a href="control.php">Control systems</a>          </li>
+          <li><a href="startup.php">Startup and Commissioning</a></li>
+          <li><a href="calib.php">Calibration</a></li>
+          <li><a href="project.php">Project Mananagement/Engineering</a></li>
+          <li><a href="addservices.php"><strong>Additional Services</strong></a></li>
+        </ul>
+        <p>
+        <p>
+        <p><br />
+          <br />
+        </div>
+	</div>
+
+	<div id="footer">
+    <table width="100%" cellpadding="20px">
+    <tr>
+    <td >
+          <img src="images/facebook-logo-webtreats.png" width="75" height="75" alt="facebook" />
+          <img src="images/twitter-webtreats.png" width="75" height="75" alt="twitter" />
+          <img src="images/linkedin4.png" width="75" height="75" alt="linkedin" />
+    <p><p><p><p>
+    <a href="index.php">Home</a> | <a href="aboutus.php">About us</a> |<a href="contactus.php">Contact us</a></td>
+    </tr>
+    <tr>
+    <td >
+    &copy; 2013. Petosam.com. All Rights Reserved.
+    </td>
+    </tr>
+    </table>
+      
+	</div>
+	<br class="clearfloat" />
+
+</div>
+ <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
+        <script type="text/javascript">
+$(function() {
+				var $oe_menu		= $('#oe_menu');
+				var $oe_menu_items	= $oe_menu.children('li');
+				var $oe_overlay		= $('#oe_overlay');
+
+                $oe_menu_items.bind('mouseenter',function(){
+					var $this = $(this);
+					$this.addClass('slided selected');
+					$this.children('div').css('z-index','9999').stop(true,true).slideDown(200,function(){
+						$oe_menu_items.not('.slided').children('div').hide();
+						$this.removeClass('slided');
+					});
+				}).bind('mouseleave',function(){
+					var $this = $(this);
+					$this.removeClass('selected').children('div').css('z-index','1');
+				});
+
+				$oe_menu.bind('mouseenter',function(){
+					var $this = $(this);
+					$oe_overlay.stop(true,true).fadeTo(200, 0.6);
+					$this.addClass('hovered');
+				}).bind('mouseleave',function(){
+					var $this = $(this);
+					$this.removeClass('hovered');
+					$oe_overlay.stop(true,true).fadeTo(200, 0);
+					$oe_menu_items.children('div').hide();
+				})
+            });
+var MenuBar1 = new Spry.Widget.MenuBar("MenuBar1", {imgRight:"SpryAssets/SpryMenuBarRightHover.gif"});
+        </script>
+</body>
+</html>

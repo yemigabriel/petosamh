524d95791f503606c3dd344fb7c7efd571040b8d
yemigabriel <yemigabriel@gmail.com>
2015-01-10 08:48:04 +0100

    h
    h

---
+++ aboutus.php
@@ -0,0 +1,335 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<link rel="shortcut icon" href="../favicon.ico" type="image/x-icon"/>
+        <link rel="stylesheet" href="css/style.css" type="text/css" media="screen"/>
+		<script src="js/cufon-yui.js" type="text/javascript"></script>
+		<script src="js/Aller.font.js" type="text/javascript"></script>
+		<script type="text/javascript">
+			Cufon.replace('ul.oe_menu div a',{hover: true});
+			Cufon.replace('h1,h2,.oe_heading');
+		</script>
+        <style type="text/css">
+			span.reference{
+				position:fixed;
+				left:0px;
+				bottom:0px;
+				background:#000;
+				width:100%;
+				font-size:10px;
+				line-height:20px;
+				text-align:right;
+				height:20px;
+				-moz-box-shadow:-1px 0px 10px #000;
+				-webkit-box-shadow:-1px 0px 10px #000;
+				box-shadow:-1px 0px 10px #000;
+			}
+			span.reference a{
+				color:#aaa;
+				text-transform:uppercase;
+				text-decoration:none;
+				margin-right:10px;
+				
+			}
+			span.reference a:hover{
+				color:#ddd;
+			}
+			.bg_img img{
+				width:100%;
+				position:fixed;
+				top:0px;
+				left:0px;
+				z-index:-1;
+			}
+			h1{
+				font-size:75px;
+				text-align:right;
+				position:absolute;
+				right:40px;
+				top:20px;
+				font-weight:normal;
+				/*text-shadow:  0 0 3px #0096ff, 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #0096ff, 0 0 70px #0096ff, 0 0 80px #0096ff, 0 0 100px #0096ff, 0 0 150px #0096ff;
+			*/}
+			h1 span{
+				display:block;
+				font-size:15px;
+				font-weight:bold;
+			}
+			h2{
+				position:absolute;
+				top:220px;
+				left:50px;
+				font-size:40px;
+				font-weight:normal;
+				/*text-shadow:  0 0 3px #f6ff00, 0 0 10px #fff, 0 0 20px #fff, 0 0 30px #fff, 0 0 40px #f6ff00, 0 0 70px #f6ff00, 0 0 80px #f6ff00, 0 0 100px #f6ff00, 0 0 150px #f6ff00;
+*/}
+		</style>
+
+
+<title>Petosam Integrated Services Ltd ::: Power Engineering</title>
+<link rel="stylesheet" type="text/css" href="./index.css" media="all" />
+<!--[if IE]>
+<style type="text/css" media="all">.borderitem {border-style:solid;}</style>
+<![endif]-->
+<script type="text/javascript" src="style/js/scripts.js"></script>
+
+<script type="text/javascript" src="Scripts/swfobject/swfobject.js"></script>
+<script src="SpryAssets/SpryMenuBar.js" type="text/javascript"></script>
+<script type="text/javascript">
+  var flashvars = {};
+  flashvars.cssSource = "piecemaker.css";
+  flashvars.xmlSource = "piecemaker.xml";
+	
+  var params = {};
+  params.play = "true";
+  params.menu = "true";
+  params.scale = "showall";
+  params.wmode = "transparent";
+  params.allowfullscreen = "true";
+  params.allowscriptaccess = "always";
+  params.allownetworking = "all";
+  
+  swfobject.embedSWF('piecemaker.swf', 'piecemaker', '850', '270', '1', null, flashvars,    
+  params, null);
+
+</script>
+
+<link href="SpryAssets/SpryMenuBarHorizontal.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body class="bodymain">
+<div id="main">
+	<div id="HeaderWrap">
+    
+	 <div id="menu">
+<img src="images/hdr1.png" width="286" height="101"  class="search" />
+	<span class="moveleft"><img src="mail.png" width="25" height="24" /><a href="mail.petosam.com">Webmail</a><br/>
+	info@petosam.com
+<br/> +234 803 641 5205</span>
+    <br class="clearfloat" />
+    
+<div class="oe_wrapper">
+			<div id="oe_overlay" class="oe_overlay"></div>
+			<ul id="oe_menu" class="oe_menu">
+				<li><a href="index.php">Home</a>
+				
+				</li>
+				<li><a href="aboutus.php">About us</a>
+					<div style="left:-111px;"><!-- -112px -->
+						<ul class="oe_full">
+							<li class="oe_heading">Who we are</li>
+							<li><p>Established since July 2001, Petosam Integrated Services Limited is an indigenous, innovative and dynamic company rendering professional Electrical &amp; Instrumentation Engineering, Civil Engineering, Procurement and Construction, Power and Safety System Control and consultancy services <a href="aboutus.php">more</a></p></li>
+						</ul>
+						
+					</div>
+				</li><li><a href="#">Services</a>
+					<div style="left:-223px;">
+						<ul>
+							<li class="oe_heading" ><a href="eleceng.php">Electrical  &amp; Instrumentation</a></li>
+							
+						</ul><ul>
+							<li class="oe_heading"><a href="pipe.php">Civil &amp; Pipeline Construction</a></li>
+							
+						</ul>
+                        <ul>
+							<li class="oe_heading"><a href="procure.php">Engineering Procurement</a></li>
+							
+							
+						</ul>
+						<ul>
+							<li class="oe_heading"><a href="powereng.php">Power Systems Engineering &amp; Support</a></li>
+							
+							
+						</ul>
+                        <ul>
+							<li class="oe_heading"><a href="lease.php">Lease &amp; Brokerage</a></li>
+							
+							
+						</ul>
+                        <ul>
+							<li class="oe_heading"><a href="engdesign.php">Engineering Design</a></li>
+							
+							
+						</ul>
+					</div>
+				</li>
+
+				<li><a href="index.php">Resources</a>
+				
+				</li>
+                
+                <li><a href="ourprojects.php">Projects</a>
+		
+				</li>
+                
+                <li><a href="index.php">QHSE</a>
+		
+				</li>
+                
+                <li><a href="index.php">Careers</a>
+		
+				</li>
+
+
+				<li><a href="contactus.php">Contact us</a>
+					<div style="left:-782px;">
+						<ul>
+							<li class="oe_heading" >Address</li>
+							<li><p>17 Hospital Road, opp. Ekpan Secondary School, Ekpan,
+Delta State, Nigeria.
+</p></li>
+						</ul>
+                        <ul>
+							<li class="oe_heading">Phone</li>
+							<li><p>+234 80209 88314<br />
++234 80364 15205</p></li>
+						</ul>
+						<ul>
+							<li class="oe_heading">Social</li>
+							<li><a href="#"><img src="images/facebook-logo-webtreats.png" width="56" height="56" alt="facebook" /></a>
+            <a href="#"><img src="images/twitter-webtreats.png" width="56" height="56" alt="twitter" /></a>
+           
+            <p>info@petosam.com</p></li>
+							
+						</ul>
+						
+					</div>
+				</li>
+<!--				<li><a href="">Stores</a>
+					<div style="left:-447px;">
+						<ul>
+							<li class="oe_heading">Europe</li>
+							<li><a href="#">Milano</a></li>
+							<li><a href="#">Paris</a></li>
+							<li><a href="#">Berlin</a></li>
+							<li><a href="#">London</a></li>
+						</ul>
+						<ul>
+							<li class="oe_heading">Asia</li>
+							<li><a href="#">Hong Kong</a></li>
+							<li><a href="#">Tokio</a></li>
+							<li><a href="#">New Delhi</a></li>
+							<li><a href="#">Beijing</a></li>
+						</ul>
+						<ul>
+							<li class="oe_heading">United States</li>
+							<li><a href="#">New York</a></li>
+							<li><a href="#">Los Angeles</a></li>
+							<li><a href="#">Seattle</a></li>
+							<li><a href="#">Miami</a></li>
+						</ul>
+					</div>
+				</li>
+-->			
+		
+			</ul>	
+		</div>
+       
+ 
+	</div>
+    </div>
+	<br class="clearfloat" />
+
+	<div id="DivWrap">
+		<div id="content">
+        <p class="headertext">About  us
+        <p>        
+        <p>Established since July 2001, Petosam Integrated Services Limited is an indigenous, innovative and dynamic company rendering professional Electrical &amp; Instrumentation Engineering, Civil Engineering, Procurement and Construction, Power and Safety System Control and consultancy services with its Head Office in Warri. We are committed to client service and equipped to conform to all known engineering standards as they apply to services in the Oil and Gas Industry as well as Maritime Industry.         
+        <p><p>        
+        <p class="headertext">Mission Statement</p>
+        We aim to be a world class company by undertaking professional Electrical &amp; Instrumentation Engineering, Civil Engineering, Procurement and Construction, Power and Safety System Maintenance Services both in the Oil and Gas Industry, Manufacturing/Producing Industry as well as Maritime industry with the highest standard of quality and operating efficiency, internationally recognized by our clients to be consistently better than that of our competitors.                                                
+        <p><p>      
+        <p class="headertext">Our Vision</p>
+          To operate safely and in harmony with global environment based on sound understanding of the regulatory framework governing every facet of our operation and act with integrity at all times.
+        <p>To maximize earnings and profits for our shareholders, provide value for money for our clients as well as provide a secure and safe environment alongside job satisfaction for our staff and, within the context of social responsibility, make positive contributions to those communities in which we operate anywhere in the world.</p>
+        <p>                        
+</div>
+        <div id="sidebar">
+        <div class="white">
+        <em><p><br /><br /><img src="images/clients1.png" width="94" height="94" />
+      <img src="images/spacer.gif" />
+      <img src="Elastislide/images/small/1.jpg" width="90" height="90" />
+      <br /><br />
+     <img src="Elastislide/images/small/2.jpg" width="90" height="90" />
+      <img src="images/spacer.gif" />
+      <img src="Elastislide/images/small/3.jpg" width="90" height="90" />
+      <img src="images/spacer.gif" />
+      <img src="Elastislide/images/small/4.jpg" width="90" height="90" />
+      <img src="images/spacer.gif" />
+      <img src="Elastislide/images/small/5.jpg" width="90" height="90" />
+      <img src="images/spacer.gif" />
+      <img src="Elastislide/images/small/6.jpg" width="90" height="90" />
+      <img src="images/spacer.gif" />
+      <img src="images/21111delta.jpg" height="90" />
+      <img src="images/spacer.gif" />
+      <img src="images/Pti-logo.jpg" height="90" />
+      <img src="images/spacer.gif" />
+      <img src="images/NDDC LOGO.jpg" height="90" />
+      <img src="images/spacer.gif" />
+      <img src="images/shell-logo.png" height="90" />
+      <img src="images/spacer.gif" />
+</p></em>
+        <br />
+       
+        <br />
+        </div>
+        
+        </div>
+	</div>
+
+	<div id="footer">
+    <table width="100%" cellpadding="20px">
+    <tr>
+    <td >
+          <img src="images/facebook-logo-webtreats.png" width="75" height="75" alt="facebook" />
+          <img src="images/twitter-webtreats.png" width="75" height="75" alt="twitter" />
+          <img src="images/linkedin4.png" width="75" height="75" alt="linkedin" />
+    <p><p><p><p>
+    <a href="index.php">Home</a> | <a href="aboutus.php">About us</a> |<a href="contactus.php">Contact us</a></td>
+    </tr>
+    <tr>
+    <td >
+    &copy; 2013. Petosam.com. All Rights Reserved.
+    </td>
+    </tr>
+    </table>
+      
+	</div>
+	<br class="clearfloat" />
+
+</div>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.3/jquery.min.js"></script>
+<script type="text/javascript">
+            $(function() {
+				var $oe_menu		= $('#oe_menu');
+				var $oe_menu_items	= $oe_menu.children('li');
+				var $oe_overlay		= $('#oe_overlay');
+
+                $oe_menu_items.bind('mouseenter',function(){
+					var $this = $(this);
+					$this.addClass('slided selected');
+					$this.children('div').css('z-index','9999').stop(true,true).slideDown(200,function(){
+						$oe_menu_items.not('.slided').children('div').hide();
+						$this.removeClass('slided');
+					});
+				}).bind('mouseleave',function(){
+					var $this = $(this);
+					$this.removeClass('selected').children('div').css('z-index','1');
+				});
+
+				$oe_menu.bind('mouseenter',function(){
+					var $this = $(this);
+					$oe_overlay.stop(true,true).fadeTo(200, 0.6);
+					$this.addClass('hovered');
+				}).bind('mouseleave',function(){
+					var $this = $(this);
+					$this.removeClass('hovered');
+					$oe_overlay.stop(true,true).fadeTo(200, 0);
+					$oe_menu_items.children('div').hide();
+				})
+            });
+        </script>
+</body>
+</html>
